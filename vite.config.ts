aee89667104287bcf4663a00d1612901364826d7
msto2 <michael.stokes.212@gmail.com>
2025-08-28 21:56:53 -0400

    it

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	server: {
-		host: '0.0.0.0', 
+		host: true, 
 		port: 5173,
 		https: true, // Enable HTTPS for external access
 		allowedHosts: ['bids.triplepoint.me'],
