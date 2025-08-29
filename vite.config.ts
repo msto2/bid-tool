f10628b510743d32d8a728e7c48fc35765fd035b
msto2 <michael.stokes.212@gmail.com>
2025-08-28 22:07:40 -0400

    sad

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ export default defineConfig({
 	server: {
 		host: true, 
 		port: 5173,
-		allowedHosts: ['bids.triplepoint.me'],
-		hmr: {
-			port: 5173,
-			host: 'localhost'
-		}
+		allowedHosts: ['bids.triplepoint.me']
 	},
 	build: {
 		rollupOptions: {
